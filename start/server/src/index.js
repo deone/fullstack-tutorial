29bf14ebdb01fb3fa1f5312b15fac3bed9945594
Oladayo Osikoya <alwaysdeone@gmail.com>
2020-01-20 15:09:30 +0000

    Reduce tab spaces.

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -4,5 +4,5 @@ const typeDefs = require('./schema')
 const server = new ApolloServer({ typeDefs })
 
 server.listen(25155).then(({ url }) => {
-    console.log(`Server ready at ${url}`)
+  console.log(`Server ready at ${url}`)
 })
